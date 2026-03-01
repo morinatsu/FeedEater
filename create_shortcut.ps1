4c20df8b9398085a9ae2453b00216ef325ef7917
morinatsu <morinatsu@gmail.com>
2026-03-01 18:47:12 +0900

    chore: remove unused create_shortcut.ps1

--- create_shortcut.ps1
+++
@@ -1,8 +0,0 @@
-$WshShell = New-Object -comObject WScript.Shell
-$DesktopPath = [Environment]::GetFolderPath("Desktop")
-$Shortcut = $WshShell.CreateShortcut("$DesktopPath\Launch FeedEater.lnk")
-$Shortcut.TargetPath = "powershell.exe"
-$Shortcut.Arguments = "-WindowStyle Hidden -Command `"cd 'C:\Users\morinatsu\projects\FeedEater'; npm run dev`""
-$Shortcut.WorkingDirectory = "C:\Users\morinatsu\projects\FeedEater"
-$Shortcut.IconLocation = "powershell.exe,0"
-$Shortcut.Save()
