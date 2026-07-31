1e5b0bcee473935c601445eac5c2f348b3a9b5c2
Natsuki Mori <morinatsu@gmail.com>
2026-08-01 08:16:53 +0900

    fix(build): remove unnecessary renderer option from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -22,8 +22,6 @@ export default defineConfig({
         // Shortcut of `build.rollupOptions.input`
         input: 'electron/preload.ts',
       },
-      // Optional: Use Node.js API in the Renderer process
-      renderer: {},
     }),
   ],
   build: {
